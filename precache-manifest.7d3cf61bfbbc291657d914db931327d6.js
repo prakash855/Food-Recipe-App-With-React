06cd202011e5e45a3c64feabb3afcdb1806ce3f0
Prakash Agrahari <sshivamprakashh301@gmail.com>
2020-08-14 17:01:59 +0530

    Updates

---
+++ precache-manifest.7d3cf61bfbbc291657d914db931327d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fbc3256ef7303a326b452535616700",
+    "url": "/Food-Recipe-App-With-React/index.html"
+  },
+  {
+    "revision": "46d0cf709f3d17e5f825",
+    "url": "/Food-Recipe-App-With-React/static/css/main.71c31e57.chunk.css"
+  },
+  {
+    "revision": "55873d71bbfbb2a5c7dd",
+    "url": "/Food-Recipe-App-With-React/static/js/2.6cee5ef2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-Recipe-App-With-React/static/js/2.6cee5ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d0cf709f3d17e5f825",
+    "url": "/Food-Recipe-App-With-React/static/js/main.a9915a5b.chunk.js"
+  },
+  {
+    "revision": "322aa9a0a982e23231f3",
+    "url": "/Food-Recipe-App-With-React/static/js/runtime-main.12d00bb6.js"
+  }
+]);
